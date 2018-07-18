ac09c80ae4fdda209d42b7d394fbdb53b3752de3
urain39 <urain39@cyfan.cf>
2018-07-18 19:43:32 +0800

    fix setup.py(keywords)

--- kngetx/__init__.py
+++ kngetx/__init__.py
@@ -6,6 +6,6 @@ __author__ = "urain39"
 __copyright__ = "Copyright 2017-2018 urain39"
 __license__ = "MIT LICENSE"
 __email__ = "urain39@qq.com"
-__version__ = '0.1.3'
+__version__ = '0.1.3.7'
 
 from .base import *
